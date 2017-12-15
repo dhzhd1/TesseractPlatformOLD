8a504a8436e5e5b41bae3fc4be537eb92f5fbe81
John Li <dhzhd1@gmail.com>
2017-12-14 17:37:04 -0800

    change password min length to 8

--- TesseractPlatform.py
+++ TesseractPlatform.py
@@ -75,7 +75,7 @@ def login():
 	if form.validate_on_submit():
 		user = User.query.filter_by(username=form.username.data).first()
 		if user:
-			if check_password_hash(user.password == form.password.data):
+			if check_password_hash(user.password, form.password.data):
 				login_user(user, remember=form.remember_me)
 				return redirect(url_for('dashboard'))
 	return render_template('login.html',
